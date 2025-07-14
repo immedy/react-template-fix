52062dd7ffe04fa118e96ea49b7eb717fdb5717b
Ragil M Rivandi <50643466+immedy@users.noreply.github.com>
2025-07-14 15:37:23 +0800

    Create patient.service.js

---
+++ src/services/patient/patient.service.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+const BASE_URL = import.meta.env.VITE_APP_URL;
+/**
+ * @param {string} token
+ * @param {number} page
+ * @param {string} search
+ * @returns {Promise<{ success : boolean, data?: any, error?: string}>}
+ */
+
+export const pasienService = async (token, page = 1, search = "")=>{
+    try{
+        const response = await axios.get(`${BASE_URL}listpasien)`,{
+            headers:{
+                Authorization: `Bearer ${token}`,
+            },
+            params:{
+                page,
+                search,
+            },
+        });
+        return {
+            success: true,
+            data: response.data,
+        };
+    } catch (error){
+        console.error("Error Saat Ambil Data Pasien", error);
+        return {
+            success: false,
+            error: error?.response?.data?.message || "Gagal Amabil Data Pasien",
+        }
+    }
+}
