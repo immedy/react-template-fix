ff8d360ba5ed306d8fa152c1c261d465445538a6
Ragil M Rivandi <50643466+immedy@users.noreply.github.com>
2025-07-14 15:35:34 +0800

    -

--- src/pages/Dashboard/detail/Pasien.jsx
+++ src/pages/Dashboard/detail/Pasien.jsx
@@ -1,99 +1,59 @@
 import { Table, TableBody, TableCell, TableHeader, TableRow } from "../../../components/ui/table";
 import Badge from "../../../components/ui/badge/Badge"
 import ComponentCard from "../../../components/common/ComponentCard";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 // Define the table data using the interface
+// const inputRef = useRef(null);
 const tableData = [
     {
         id: 1,
-        user: {
-            image: "/images/user/user-17.jpg",
-            name: "Lindsey Curtis",
-            role: "Web Designer"
-        },
-        projectName: "Agency Website",
-        team: {
-            images: [
-                "/images/user/user-22.jpg",
-                "/images/user/user-23.jpg",
-                "/images/user/user-24.jpg"
-            ]
-        },
-        budget: "3.9K",
-        status: "Active"
+        nama: "Lindsay",
+        norm: "000021",
+        jenis_kelamin: "Laki-Laki",
+        tempat_lahir: "Kota Bangun",
+        tanggal_lahir: "1 Juni 1992",
+        alamat: "Jalan Kudunggaa gg 4 rt 10 rw 15",
     },
-    {
-        id: 2,
-        user: {
-            image: "/images/user/user-18.jpg",
-            name: "Kaiya George",
-            role: "Project Manager"
-        },
-        projectName: "Technology",
-        team: {
-            images: ["/images/user/user-25.jpg", "/images/user/user-26.jpg"]
-        },
-        budget: "24.9K",
-        status: "Pending"
-    },
-    {
-        id: 3,
-        user: {
-            image: "/images/user/user-17.jpg",
-            name: "Zain Geidt",
-            role: "Content Writing"
-        },
-        projectName: "Blog Writing",
-        team: {
-            images: ["/images/user/user-27.jpg"]
-        },
-        budget: "12.7K",
-        status: "Active"
-    },
-    {
-        id: 4,
-        user: {
-            image: "/images/user/user-20.jpg",
-            name: "Abram Schleifer",
-            role: "Digital Marketer"
-        },
-        projectName: "Social Media",
-        team: {
-            images: [
-                "/images/user/user-28.jpg",
-                "/images/user/user-29.jpg",
-                "/images/user/user-30.jpg"
-            ]
-        },
-        budget: "2.8K",
-        status: "Cancel"
-    },
-    {
-        id: 5,
-        user: {
-            image: "/images/user/user-21.jpg",
-            name: "Carla George",
-            role: "Front-end Developer"
-        },
-        projectName: "Website",
-        team: {
-            images: [
-                "/images/user/user-31.jpg",
-                "/images/user/user-32.jpg",
-                "/images/user/user-33.jpg"
-            ]
-        },
-        budget: "4.5K",
-        status: "Active"
-    }
 ]
 
 export default function Pasien() {
     return (
         <>
             <div className="space-y-6">
-                <ComponentCard title="Pasien">
+                <ComponentCard title="Pasien" headerRight={
+                    <form>
+                        <div className="relative">
+                            <span className="absolute -translate-y-1/2 pointer-events-none left-4 top-1/2">
+                                <svg
+                                    className="fill-gray-500 dark:fill-gray-400"
+                                    width="20"
+                                    height="20"
+                                    viewBox="0 0 20 20"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        clipRule="evenodd"
+                                        d="M3.04175 9.37363C3.04175 5.87693 5.87711 3.04199 9.37508 3.04199C12.8731 3.04199 15.7084 5.87693 15.7084 9.37363C15.7084 12.8703 12.8731 15.7053 9.37508 15.7053C5.87711 15.7053 3.04175 12.8703 3.04175 9.37363ZM9.37508 1.54199C5.04902 1.54199 1.54175 5.04817 1.54175 9.37363C1.54175 13.6991 5.04902 17.2053 9.37508 17.2053C11.2674 17.2053 13.003 16.5344 14.357 15.4176L17.177 18.238C17.4699 18.5309 17.9448 18.5309 18.2377 18.238C18.5306 17.9451 18.5306 17.4703 18.2377 17.1774L15.418 14.3573C16.5365 13.0033 17.2084 11.2669 17.2084 9.37363C17.2084 5.04817 13.7011 1.54199 9.37508 1.54199Z"
+                                        fill=""
+                                    />
+                                </svg>
+                            </span>
+                            <input
+                                type="text"
+                                placeholder="Search or type command..."
+                                className="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-200 bg-transparent py-2.5 pl-12 pr-14 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-800 dark:bg-gray-900 dark:bg-white/[0.03] dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 xl:w-[430px]"
+                            />
+
+                            <button className="absolute right-2.5 top-1/2 inline-flex -translate-y-1/2 items-center gap-0.5 rounded-lg border border-gray-200 bg-gray-50 px-[7px] py-[4.5px] text-xs -tracking-[0.2px] text-gray-500 dark:border-gray-800 dark:bg-white/[0.03] dark:text-gray-400">
+                                <span> Cari </span>
+                            </button>
+                        </div>
+                    </form>
+                }>
+
                     <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
                         <div className="max-w-full overflow-x-auto">
                             <Table>
@@ -104,96 +64,80 @@ export default function Pasien() {
                                             isHeader
                                             className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
                                         >
-                                            User
+                                            Nama Pasien
                                         </TableCell>
                                         <TableCell
                                             isHeader
                                             className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
                                         >
-                                            Project Name
+                                            No Rekam Medis
                                         </TableCell>
                                         <TableCell
                                             isHeader
                                             className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
                                         >
-                                            Team
+                                            Tempat, Tanggal Lahir
                                         </TableCell>
                                         <TableCell
                                             isHeader
                                             className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
                                         >
-                                            Status
+                                            Jenis Kelamin
                                         </TableCell>
                                         <TableCell
                                             isHeader
                                             className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
                                         >
-                                            Budget
+                                            Aksi
                                         </TableCell>
                                     </TableRow>
                                 </TableHeader>
 
                                 {/* Table Body */}
                                 <TableBody className="divide-y divide-gray-100 dark:divide-white/[0.05]">
-                                    {tableData.map(order => (
-                                        <TableRow key={order.id}>
+                                    {tableData.map(Pasien => (
+                                        <TableRow key={Pasien.id}>
                                             <TableCell className="px-5 py-4 sm:px-6 text-start">
                                                 <div className="flex items-center gap-3">
-                                                    <div className="w-10 h-10 overflow-hidden rounded-full">
-                                                        <img
-                                                            width={40}
-                                                            height={40}
-                                                            src={order.user.image}
-                                                            alt={order.user.name}
-                                                        />
-                                                    </div>
                                                     <div>
                                                         <span className="block font-medium text-gray-800 text-theme-sm dark:text-white/90">
-                                                            {order.user.name}
+                                                            {Pasien.nama}
                                                         </span>
                                                         <span className="block text-gray-500 text-theme-xs dark:text-gray-400">
-                                                            {order.user.role}
+                                                            {Pasien.alamat}
                                                         </span>
                                                     </div>
                                                 </div>
                                             </TableCell>
                                             <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
-                                                {order.projectName}
+                                                {Pasien.norm}
                                             </TableCell>
                                             <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
                                                 <div className="flex -space-x-2">
-                                                    {order.team.images.map((teamImage, index) => (
-                                                        <div
-                                                            key={index}
-                                                            className="w-6 h-6 overflow-hidden border-2 border-white rounded-full dark:border-gray-900"
-                                                        >
-                                                            <img
-                                                                width={24}
-                                                                height={24}
-                                                                src={teamImage}
-                                                                alt={`Team member ${index + 1}`}
-                                                                className="w-full size-6"
-                                                            />
-                                                        </div>
-                                                    ))}
+                                                    <span className="block font-medium text-gray-800 text-theme-sm dark:text-white/90">
+                                                        {Pasien.tempat_lahir},  {Pasien.tanggal_lahir}
+                                                    </span>
                                                 </div>
                                             </TableCell>
                                             <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
-                                                <Badge
-                                                    size="sm"
-                                                    color={
-                                                        order.status === "Active"
-                                                            ? "success"
-                                                            : order.status === "Pending"
-                                                                ? "warning"
-                                                                : "error"
-                                                    }
-                                                >
-                                                    {order.status}
-                                                </Badge>
+                                                 {Pasien.jenis_kelamin}
                                             </TableCell>
                                             <TableCell className="px-4 py-3 text-gray-500 text-theme-sm dark:text-gray-400">
-                                                {order.budget}
+                                                <div className="relative group inline-block">
+                                                    <button
+
+                                                        className="flex items-center justify-center gap-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
+                                                    >
+                                                        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                                            <path fill-rule="evenodd" d="M3.559 4.544c.355-.35.834-.544 1.33-.544H19.11c.496 0 .975.194 1.33.544.356.35.559.829.559 1.331v9.25c0 .502-.203.981-.559 1.331-.355.35-.834.544-1.33.544H15.5l-2.7 3.6a1 1 0 0 1-1.6 0L8.5 17H4.889c-.496 0-.975-.194-1.33-.544A1.868 1.868 0 0 1 3 15.125v-9.25c0-.502.203-.981.559-1.331ZM7.556 7.5a1 1 0 1 0 0 2h8a1 1 0 0 0 0-2h-8Zm0 3.5a1 1 0 1 0 0 2H12a1 1 0 1 0 0-2H7.556Z" clip-rule="evenodd" />
+                                                        </svg>
+
+                                                        <span className="absolute z-10 bottom-full mb-2 hidden w-max rounded bg-black px-2 py-1 text-xs text-white opacity-0 group-hover:opacity-100 group-hover:block transition">
+                                                            Daftar
+                                                        </span>
+                                                    </button>
+
+                                                </div>
                                             </TableCell>
                                         </TableRow>
                                     ))}
